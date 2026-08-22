deee47ed0de58a2317a4bbf99bc0c84f2dece878
rfreel <ryanfreel@gmail.com>
2026-08-22 16:39:45 -0700

    feat(cleanroom): export provider model layer

--- cleanroom/src/index.js
+++ cleanroom/src/index.js
@@ -1,3 +1,4 @@
 export { runAgent, AgentAbortError, MaxStepsError } from './agent.js';
 export { ToolRegistry } from './tools.js';
 export { Role, textMessage, toolCall } from './types.js';
+export { createProviderModel, ScriptedProvider, ProviderError } from './model.js';
